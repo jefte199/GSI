ae5e01cf47bb2c11ed1192843679aa5e667b3b88
Jefte <jefte199@gmail.com>
2023-05-06 23:45:48 -0300

    feat: create price format

---
+++ src/util/priceFormat.ts
@@ -0,0 +1,7 @@
+export function priceFormat(value: number) {
+  const formattedValue = value.toLocaleString('pt-BR', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+  return formattedValue.replace(',', '.');
+}
