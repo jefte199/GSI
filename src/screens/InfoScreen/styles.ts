b84f7e45b41b37cff143cd6f322bf4347fc0a454
Jefte <jefte199@gmail.com>
2023-05-22 10:36:34 -0300

    feat: use react-native-safe-area-context

--- src/screens/InfoScreen/styles.ts
+++ src/screens/InfoScreen/styles.ts
@@ -1,3 +1,4 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
 export type StyleProps = 'PRIMARY' | 'SECONDARY' | 'TERTIARY';
@@ -6,7 +7,7 @@ type Props = {
   type: StyleProps
 }
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   padding: 24px;
 `;
