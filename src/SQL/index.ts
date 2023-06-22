bb8e85c964b0f9cf3e77902eaaa489c5e9ae6b72
Jefte <jefte199@gmail.com>
2023-06-22 10:32:55 -0300

    feat: update sql

--- src/SQL/index.ts
+++ src/SQL/index.ts
@@ -1,32 +1,59 @@
 import { db } from '../database';
-import { SQLite } from '../types/SQLite';
-
-export const createTable = () => {
-  db.transaction((transaction) => {
-    transaction.executeSql(
-      `CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        email TEXT
-      );`,
-      [],
-      (_, result) => {
-        console.log('Table created successfully');
-      },
-      (_, error) => {
-        console.log('Error creating table:', error);
-      }
-    );
-  });
-};
+import { House } from '../types/House';
 
 export const sql = {
-  createUser: (user: SQLite) => {
+  createTable: () => {
     return new Promise((resolve, reject) => {
       db.transaction((transaction) => {
         transaction.executeSql(
-          'INSERT INTO users (name, email) VALUES (?, ?);',
-          [user.name, user.email],
+          `CREATE TABLE IF NOT EXISTS houses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            newHouse TEXT,
+            rented TEXT,
+            selectedDate TEXT,
+            garage TEXT,
+            price INTEGER,
+            address TEXT,
+            neighborhood TEXT,
+            bathroom INTEGER,
+            rooms INTEGER,
+            area INTEGER,
+            comment TEXT,
+            imageURLs TEXT,
+            contactName TEXT,
+            contactEmail TEXT,
+            contactPhone TEXT,
+            contactAddress TEXT
+          );
+          `, [],
+          (_, result) => {
+            console.log('Table created successfully');
+          },
+          (_, error) => {
+            console.log('Error creating table:', error);
+          }
+        );
+      });
+    });
+  },
+
+  createHouse: ({
+    newHouse, rented, selectedDate, garage, price, address, neighborhood, bathroom,
+    rooms, area, comment, imageUrlString, contactName, contactEmail, contactPhone, contactAddress, }: House) => {
+    return new Promise((resolve, reject) => {
+      db.transaction((transaction) => {
+        transaction.executeSql(
+          `INSERT INTO houses (
+            newHouse, rented, selectedDate, garage, price, address, neighborhood,
+            bathroom, rooms, area, comment, imageUrls, contactName, contactEmail, contactPhone,
+            contactAddress
+          )
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
+          [
+            newHouse, rented, selectedDate, garage, price, address, neighborhood, bathroom,
+            rooms, area, comment, JSON.stringify(imageUrlString), // Converte o array de URLs para uma string JSON
+            contactName, contactEmail, contactPhone, contactAddress,
+          ],
           (_, { rowsAffected, insertId }) => {
             if (rowsAffected > 0) {
               resolve(insertId);
@@ -42,18 +69,18 @@ export const sql = {
     });
   },
 
-  getUserById: (userId: number): Promise<User> => {
-    return new Promise<SQLite>((resolve, reject) => {
+  getHouseById: (houseId: number): Promise<House> => {
+    return new Promise<House>((resolve, reject) => {
       db.transaction((transaction) => {
         transaction.executeSql(
-          'SELECT * FROM users WHERE id = ?;',
-          [userId],
+          'SELECT * FROM houses WHERE id = ?;',
+          [houseId],
           (_, { rows }) => {
             if (rows.length > 0) {
-              const user = rows.item(0) as SQLite; // Obtém o primeiro registro encontrado
-              resolve(user);
+              const house = rows.item(0) as House; // Obtém o primeiro registro encontrado
+              resolve(house);
             } else {
-              reject(new Error('User not found'));
+              reject(new Error('house not found'));
             }
           },
           (_, error) => {
@@ -64,19 +91,18 @@ export const sql = {
     });
   },
 
-  getUser: (): Promise<SQLite> => {
-    return new Promise<SQLite>((resolve, reject) => {
+  getHouse: () => {
+    return new Promise<House>((resolve, reject) => {
       db.transaction((transaction) => {
         transaction.executeSql(
-          'SELECT * FROM users;',
+          'SELECT * FROM houses;',
           [],
           (_, { rows }) => {
             if (rows.length > 0) {
-              const user = rows.item(0) as SQLite; // Obtém o primeiro registro encontrado
-              resolve(user);
-              console.log(rows._array[5].name)
+              const house = rows.item(0) as House; // Obtém o primeiro registro encontrado
+              resolve(house);
             } else {
-              reject(new Error('User not found'));
+              reject(new Error('House not found'));
             }
           },
           (_, error) => {
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,79 @@
+import { useEffect } from 'react';
 import { FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+// Style
 import { Container, Line, OnClick } from './styles';
-
+// Components
 import { Header } from '../../Components/Header';
 import { Card } from '../../Components/Card';
 import { Button } from '../../Components/Button';
 import { ListEmpty } from "../../Components/ListEmpty";
+// SQL
+import { sql } from '../../SQL';
+import { House } from '../../types/House';
 
 export function Home() {
+  const house: House = {
+    newHouse: 'Yes',
+    rented: 'No',
+    selectedDate: '2023-05-17',
+    garage: 'Yes',
+    price: 200000,
+    address: '123 Main St',
+    neighborhood: 'Example Neighborhood',
+    bathroom: 2,
+    rooms: 3,
+    area: 150,
+    comment: 'Spacious house with a backyard',
+    imageUrlString: ['https://example.com/image1.jpg', 'https://example.com/image2.jpg'],
+    contactName: 'John Doe',
+    contactEmail: 'johndoe@example.com',
+    contactPhone: '123-456-7890',
+    contactAddress: '456 Elm St',
+  };
+
+  function createTable() {
+    sql.createTable()
+      .then((house) => {
+        console.log('House:', house);
+      })
+      .catch((error) => {
+        console.error('Erro:', error);
+      });
+  }
+
+  function create() {
+    sql.createHouse(house)
+      .then((house) => {
+        console.log('House:', house);
+      })
+      .catch((error) => {
+        console.error('Erro:', error);
+      });
+  }
+
+  function select() {
+    sql.getHouse()
+      .then((house) => {
+        console.log('House:', house);
+      })
+      .catch((error) => {
+        console.error('Erro:', error);
+      });
+  }
+
+  useEffect(() => {
+    createTable();
+    navigation.navigate('add_imovel');
+  }, []);
+
   const imgCasa = require('../../assets/Casa.jpg');
   const data = [{ id: 1, title: 'casa 1', location: 'aquela rua', price: '15000R$' }, { id: 2, title: 'casa 2', location: 'aquela rua', price: '95000R$' }, { id: 3, title: 'casa 3', location: 'aquela rua', price: '150R$' }]
 
   const navigation = useNavigation();
 
   function handleAdd() {
+    select();
     navigation.navigate('add_imovel');
   }
 
