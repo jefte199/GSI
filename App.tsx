86fe47cff003f3e78098c80fcf4ef1d6126f03ab
mateusfelixdias <felixmateus618@gmail.com>
2023-08-12 10:14:02 -0300

    feat: added fonts roboto

--- App.tsx
+++ App.tsx
@@ -2,10 +2,19 @@ import theme from './src/themes';
 
 import { Routes } from './src/routes';
 
+import { useFonts } from 'expo-font';
+
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    ['Roboto-700']: require('./src/assets/fonts/Roboto-Bold.ttf'),
+    ['Roboto-400']: require('./src/assets/fonts/Roboto-Regular.ttf'),
+  });
+
+  if (!isFontsLoaded) return null;
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="dark" />
