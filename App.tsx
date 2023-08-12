07ddb5ae4aebe08aecbbb138bdbda5cef47edac8
mateusfelixdias <felixmateus618@gmail.com>
2023-08-12 10:15:01 -0300

    refactor: organized importations

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import theme from './src/themes';
 
-import { Routes } from './src/routes';
-
 import { useFonts } from 'expo-font';
 
+import { Routes } from './src/routes';
+
 import { StatusBar } from 'expo-status-bar';
+
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
