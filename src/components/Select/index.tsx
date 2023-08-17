c9f188f68935e724cd00918c7cfd8bd83e276a3f
mateusfelixdias <felixmateus618@gmail.com>
2023-08-17 18:52:37 -0300

    feat: adding error message styling

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,32 +1,46 @@
-import { Picker } from '@react-native-picker/picker';
+import { Text } from '../Text';
+
+import { forwardRef } from 'react';
 
 import { Container } from './styles';
 
+import { Picker } from '@react-native-picker/picker';
+
 interface ChildComponentProps {
-  List: string[];
-  SelectedOption: string;
+  list: string[];
+  error?: string;
+  selectedOption: string;
   setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export function Select({
-  List,
-  SelectedOption,
-  setSelectedOption,
-}: ChildComponentProps) {
+export const Select = forwardRef((props: ChildComponentProps, ref) => {
+  const { error = '', list, selectedOption, setSelectedOption } = props;
+
+  const isError = error.trim().length > 0;
+  const isOptionSelected = selectedOption.length;
+
   const handleSelect = (value: string) => {
     setSelectedOption(value);
   };
 
   return (
-    <Container>
-      <Picker
-        selectedValue={SelectedOption}
-        onValueChange={(itemValue) => handleSelect(itemValue)}
-      >
-        {List.map((item) => (
-          <Picker.Item key={item} label={item} value={item} />
-        ))}
-      </Picker>
-    </Container>
+    <>
+      <Container isError={isError && !isOptionSelected}>
+        <Picker
+          selectedValue={selectedOption}
+          onValueChange={(itemValue) => handleSelect(itemValue)}
+        >
+          {list.map((item) => (
+            <Picker.Item key={item} label={item} value={item} />
+          ))}
+        </Picker>
+      </Container>
+
+      {isError && !isOptionSelected && (
+        <Text weight="700" color="#ec4561" style={{ marginTop: 4 }}>
+          {error}
+        </Text>
+      )}
+    </>
   );
-}
+});
--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -4,4 +4,8 @@ export const Container = styled.View`
   padding: 8px;
   border-radius: 8px;
   background-color: #fff;
+
+  border: ${({ isError }: { isError: boolean }) => {
+    return isError ? '1px solid #ec4561' : 'none';
+  }};
 `;
