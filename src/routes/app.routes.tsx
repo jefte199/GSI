f8cee0bccc4f72f6c5fed97eb6cc41157733d44f
mateusfelixdias <felixmateus618@gmail.com>
2023-08-17 13:52:32 -0300

    feat: added route update

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,7 @@
 import { Home } from '../screens/Home';
 import { InfoScreen } from '../screens/InfoScreen';
-import { AddPropertyForm } from '../screens/AddPropertyForm';
+import { AddProperty } from '../screens/AddProperty';
+import { UpdateProperty } from '../screens/UpdateProperty';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -11,9 +12,11 @@ export function AppRoutes() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="home" component={Home} />
 
-      <Stack.Screen name="add_imovel" component={AddPropertyForm} />
-
       <Stack.Screen name="info" component={InfoScreen} />
+
+      <Stack.Screen name="update" component={UpdateProperty} />
+
+      <Stack.Screen name="add_imovel" component={AddProperty} />
     </Stack.Navigator>
   );
 }
