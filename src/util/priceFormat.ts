73ddedc4e39cdc831812fb2c4a03254f53b4cfba
mateusfelixdias <felixmateus618@gmail.com>
2023-08-17 13:47:45 -0300

    refactor: remove util

--- src/util/priceFormat.ts
+++
@@ -1,8 +0,0 @@
-export function priceFormat(value: number) {
-  const formattedValue = value.toLocaleString('pt-BR', {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  });
-  
-  return formattedValue.replace(',', '.');
-}
