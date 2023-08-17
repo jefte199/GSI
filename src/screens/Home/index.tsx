1677212fb1ba345ea240ed92fcfb799bd08724f2
mateusfelixdias <felixmateus618@gmail.com>
2023-08-17 13:53:33 -0300

    feat: added improvements

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,128 +1,101 @@
+import { Container, Line } from './styles';
+
 import { useEffect, useState } from 'react';
-import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-// Style
-import { Container, Line, OnClick } from './styles';
-// Components
-import { Header } from '../../Components/Header';
-import { Card } from '../../Components/Card';
-import { Button } from '../../Components/Button';
-import { ListEmpty } from "../../Components/ListEmpty";
-// SQL
-import { sql } from '../../SQL';
-// types
-import { House } from '../../types/House';
-//import { card } from '../../types/card';
+import { FlatList, TouchableOpacity } from 'react-native';
 
-export function Home() {
-  const house: House = {
-    id: 0,
-    newHouse: 'Yes',
-    rented: 'No',
-    selectedDate: '2023-05-17',
-    garage: 'Yes',
-    price: 200000,
-    address: '123 Main St',
-    neighborhood: 'Example Neighborhood',
-    bathroom: 2,
-    rooms: 3,
-    area: 150,
-    comment: 'Spacious house with a backyard',
-    imageUrlString: ['https://example.com/image1.jpg', 'https://example.com/image2.jpg'],
-    contactName: 'John Doe',
-    contactEmail: 'johndoe@example.com',
-    contactPhone: '123-456-7890',
-    contactAddress: '456 Elm St',
-  };
-  // States
-  const [data, setData] = useState<House[]>([]);
+import { Card } from '../../components/Card';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Loading } from '../../components/Loading';
+import { ListEmpty } from '../../components/ListEmpty';
 
-  useEffect(() => {
-    createTable();
-    select();
-  }, []);
+import { select } from '../../services/select';
+import { createTable } from '../../services/createTable';
 
-  const createTable = async () => {
-    await sql.createTable()
-      .then((house) => {
-        console.log('House:', house);
-      })
-      .catch((error) => {
-        console.error('Erro:', error);
-      });
-  }
+import { HouseWithId } from '../../types/House';
 
-  const create = async () => {
-    await sql.createHouse(house)
-      .then((house) => {
-        console.log('House:', house);
-      })
-      .catch((error) => {
-        console.error('Erro:', error);
-      });
-  }
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-  const deleteHouseById = async (id: number) => {
-    try {
-      await sql.deleteHouse(id);
-      console.log('House deleted successfully.');
-    } catch (error) {
-      console.error('Failed to delete house:', error);
-    }
-  };
+type TypeNavigation = NativeStackNavigationProp<AppStackParamList>;
 
-  const select = async () => {
-    await sql.getHouse()
-      .then((house) => {
-        setData(house)
-        return house
-      })
-      .catch((error) => {
-        return error
-      });
-  }
+interface HomeProps {
+  navigation: TypeNavigation;
+}
 
-  const navigation = useNavigation();
+export function Home({ navigation }: HomeProps) {
+  const [isLoading, setIsLoading] = useState(false);
 
-  async function handleAdd() {
+  const [data, setData] = useState<HouseWithId[]>([]);
+
+  function goPageAddProperty() {
     navigation.navigate('add_imovel');
   }
 
-  function handleInfo(info: House | undefined) {
-    navigation.navigate('info', { home: info });
+  function goPageInfo(house: HouseWithId) {
+    navigation.navigate('info', { house });
   }
 
-  const imgCasa = require('../../assets/Casa.jpg');
-  function myCard(item: House) {
+  const handleHouses = async () => {
+    setIsLoading(true);
+
+    const house = await select();
+
+    setData(house ?? []);
+
+    setIsLoading(false);
+  };
+
+  function renderCard(item: HouseWithId) {
     return (
-      <OnClick onPress={() => handleInfo(item)}>
+      <TouchableOpacity onPress={() => goPageInfo(item)}>
         <Card
-          imageSource={imgCasa}
           comment={item.comment}
           location={item.address}
-          price={`${item.price}`} />
-      </OnClick>
-    )
+          price={`${item.price}`}
+          imageSource={item.imageUrl}
+        />
+      </TouchableOpacity>
+    );
   }
+
+  useEffect(() => {
+    createTable();
+
+    navigation.addListener('focus', handleHouses);
+
+    return () => {
+      navigation.removeListener('focus', handleHouses);
+    };
+  }, []);
+
+  const flex = data.length === 0 ? 1 : undefined;
 
   return (
     <Container>
-      <Header showBackButton={false} />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header navigation={navigation} showBackButton={false} />
 
-      <Line />
+          <Line />
 
-      <FlatList
-        data={data}
-        renderItem={({ item }) => myCard(item)}
-        keyExtractor={(item) => `${item.id}`}
-        contentContainerStyle={data.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => <ListEmpty message="Cadastre o primeiro Imovel" />}
-      />
+          <FlatList
+            data={data}
+            keyExtractor={({ id }) => `${id}`}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => renderCard(item)}
+            contentContainerStyle={{ flex, paddingTop: 10, paddingBottom: 50 }}
+            ListEmptyComponent={() => (
+              <ListEmpty message="Cadastre o seu primeiro imóvel" />
+            )}
+          />
 
-      <Button
-        title='Cadastrar novo Imovel'
-        type='PRIMARY'
-        onPress={handleAdd} />
+          <Button type="PRIMARY" onPress={goPageAddProperty}>
+            Cadastrar um novo imóvel
+          </Button>
+        </>
+      )}
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,30 +1,12 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled(SafeAreaView)`
+export const Container = styled.SafeAreaView`
   flex: 1;
-  //background-color: ${({ theme }) => theme.COLORS.GRAY_400};
   padding: 24px;
-  `;
-
-export const OnClick = styled(TouchableOpacity)`
-`;
-
-export const Title = styled.Text`
-  color: #FFF;
-  font-size: 32;
 `;
 
 export const Line = styled.View`
   height: 1px;
+  margin: 0 16px;
   background-color: #ddd;
-  margin-horizontal: 16px;
-`;
-
-export const FlatListContainer = styled.FlatList`
-  flex: 1;
-  width: 100%;
-  background-color: #f0f0f0;
-  padding: 16px;
 `;
