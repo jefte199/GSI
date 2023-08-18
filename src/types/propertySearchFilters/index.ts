3ae28cfc9051df1fd0756f1d7d9a8e32f07a6461
mateusfelixdias <felixmateus618@gmail.com>
2023-08-18 11:26:33 -0300

    feat: added type of property search filters

---
+++ src/types/propertySearchFilters/index.ts
@@ -0,0 +1,11 @@
+export interface PropertySearchFilters {
+  area: number;
+  price: number;
+  rooms: number;
+  garage: number;
+  rented: string;
+  address: string;
+  bathroom: number;
+  newHouse: string;
+  neighborhood: string;
+}
